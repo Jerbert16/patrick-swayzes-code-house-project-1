4ad784f0f7add70284b67f9cddb7f878dc4ec580
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-02-06 19:52:01 -0700

    inserted input and button into html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var applicationId = "530a1429-db0d-4587-adb1-ded19a7d404d";
 var applicationSecret =
   "b61037a5c464044d27334b3dd0f410339546c8ff10ab50eebcce8b488b5439217224c87383070db4a719e49e54f3788da3c23a76362f4e57458eb7155538ab58154b897ef27bffefd7aa5f592237c3f43327b5a8b4c82efca177e8a995816f78c8da59077974d86e840c98de251e9c07";
 var baseurlPositions =
-  "https://api.astronomyapi.com/api/v2/bodies/positions?longitude=-84.39733&latitude=33.775867&elevation=1&from_date=2023-02-03&to_date=2023-02-03&time=05%3A51%3A49";
+  "https://api.astronomyapi.com/api/v2/bodies/positions?longitude=122.4194&latitude=37.7749&elevation=1&from_date=2023-02-03&to_date=2023-02-03&time=05%3A51%3A49";
 // here in baseurl we are looking Planetary Positions endpoint and  Longitude,Latitude,From Date,To Date,and Time are hardcoded we have to find a way a user to enter or get their location
 //the other end points are Star Charts,Moon Phase.search
 const hash = btoa(`${applicationId}:${applicationSecret}`);
@@ -42,7 +42,7 @@ let mydata = {
     headingColor: "#ffffff",
     textColor: "#ffffff",
   },
-  observer: { latitude: 33.775867, longitude: -84.39733, date: "2023-02-04" },
+  observer: { latitude: 37.7749, longitude: -84.39733, date: "2023-02-04" },
   view: { type: "landscape-simple" },
 };
 
@@ -66,7 +66,7 @@ const baseUrlStarChart =
   "https://api.astronomyapi.com/api/v2/studio/star-chart";
 let mydatatwo = {
   style: "inverted",
-  observer: { latitude: 33.775867, longitude: -84.39733, date: "2023-02-04" },
+  observer: { latitude: 31.9523, longitude: 115.8613, date: "2023-02-06" },
   view: { type: "constellation", parameters: { constellation: "ori" } },
 };
 fetch(baseUrlStarChart, {
